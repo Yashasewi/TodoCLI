1a747c72865455a156f000e82b94a69e481111f4
Yashasewi <32950332+Yashasewi@users.noreply.github.com>
2025-06-13 18:44:56 +0530

    Integrate save package for task management in main function, enabling task addition and marking tasks as done

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"todo_list/save"
 	"todo_list/todo"
 )
 
@@ -11,7 +12,7 @@ const CONFIG_PATH = "./config.json"
 
 func main() {
 	command_line_args := os.Args[1:]
-	var tasks []string
+	tasks := save.AllTask(FILE_PATH)
 
 	if len(command_line_args) == 0 {
 		fmt.Println("No command line arguments provided.")
@@ -21,6 +22,12 @@ func main() {
 	switch command_line_args[0] {
 	case "--add", "-a":
 		tasks = todo.CreateTask(tasks, command_line_args[1])
+		res, err := save.SaveFile(FILE_PATH, command_line_args[1])
+		if err != nil {
+			fmt.Println(res, err)
+		} else {
+			fmt.Println(res)
+		}
 		fmt.Printf("Task added: %s\n", command_line_args[1])
 
 	case "--delete", "-d":
@@ -31,13 +38,17 @@ func main() {
 		} else {
 			fmt.Println("Task not found")
 		}
+	case "--done", "-do":
+		doneTask := command_line_args[1]
+		todo.MarkDone(FILE_PATH, doneTask)
+		fmt.Println("This task is now mark as done :", doneTask)
+
 	case "--print", "-p":
-		todo.PrintTasks(tasks)
+		todo.PrintTasks(FILE_PATH)
 	case "--help", "-h", "?":
 		todo.PrintHelp()
 	default:
 		fmt.Print("Please use the --help or -h or ? to view the use cases")
 	}
 
-	fmt.Println("Current tasks:", tasks)
 }
